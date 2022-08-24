4cd50a35e1921d12e3caed5461bfa4777643a32c
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-08-24 09:30:31 +0530

    Add files via upload

---
+++ two_dim_arrays_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout <<"Representation of 2D array" << endl;
+    int arr[2][3] = {{1,2,3},{4,5,6}};
+    for(int i = 0; i<2;i++){
+        for(int j =0; j<3;j++){
+            cout <<"arr[" << i << "][" << j << "] = " <<arr[i][j] << endl;
+        }
+    }
+    return 0;
+}
