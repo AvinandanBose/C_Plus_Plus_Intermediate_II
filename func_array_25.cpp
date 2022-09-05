03717d4a76dbd6ec7410c3af3804c5d6b67b679b
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-09-06 00:15:34 +0530

    Add files via upload

--- func_array_25.cpp
+++ func_array_25.cpp
@@ -52,6 +52,10 @@ Label:
         for (int i = 0; i <size; i++)
         {
             array[i] = array[i + 1];
+            if ( i == size - 1)
+            {
+                break;
+            }
         }
         size = size - 1;
         cout << "Array after deleting the element: " << endl;
@@ -79,7 +83,9 @@ Label:
  *                 for int i= 0; i< 3; i++ :
  *                array[0] = array[0+1] = array[1] = 2
  *               array[1] = array[1+1] = array[2] = 3
- *              array[2] = array[2+1] = array[3] = garbage value
+ *             
+ *          And, array[2] = array[2+1] = array[3] = garbage value [Which is not needed]
+ *                        Hence: if(i==3-1) break;
  * 
  * Size  = Size -1 = 2
  * 
