7b0f874b187881670162242de1dcdc0be197930c
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-09-19 10:33:03 +0530

    Add files via upload

---
+++ twodim_recur_22.cpp
@@ -0,0 +1,78 @@
+// Diagonal of a Sqaure  Matrix using Recursion
+
+#include <iostream>
+int input(int[][10], int, int);
+void display(int, int, int[][10]);
+int showDiagonalOfMatrix(int[][10], int, int, int, int);
+using namespace std;
+int main()
+{
+
+    int a[10][10];
+    int m, n;
+    cout << "Enter row  for Matrix A: " << endl;
+    cin >> m;
+    cout << "Enter columns  for Matrix A: " << endl;
+    cin >> n;
+    if (m == n)
+    {
+        cout << "Enter the elements of Matrix A: " << endl;
+        input(a, m, n);
+        cout << "Displaying the Matrix A: " << endl;
+        display(m, n, a);
+        cout << "Displaying the Diagonal Of A Matrix: " << endl;
+        showDiagonalOfMatrix(a, m, n, 0, 0);
+    }
+    else
+    {
+        cout << "Matrix is not square matrix" << endl;
+    }
+
+    return 0;
+}
+int input(int array[][10], int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cout << "arr[" << i << "][" << j << "] = ";
+            cin >> array[i][j];
+        }
+    }
+    return array[rows][10];
+}
+
+void display(int rows, int columns, int array[][10])
+{
+    for (int i = 0; i < rows; i++)
+    {
+        cout << "|";
+        for (int j = 0; j < columns; j++)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << "|" << endl;
+    }
+}
+int showDiagonalOfMatrix(int arr[][10], int rows, int columns, int i, int j)
+{
+
+    if (i == rows)
+    {
+        return 0;
+    }
+    if (j == columns)
+    {
+        cout << endl;
+        return showDiagonalOfMatrix(arr, rows, columns, i + 1, 0);
+    }
+    if (i==j)
+    {
+        cout << arr[i][j] << " ";
+    }
+    else{
+        cout << " ";
+    }
+    return showDiagonalOfMatrix(arr, rows, columns, i, j + 1);
+}
---
+++ twodim_recur_23.cpp
@@ -0,0 +1,80 @@
+// Diagonal  of A Square Matrix using Recursion
+
+#include <iostream>
+int input(int[][10], int, int);
+void display(int, int, int[][10]);
+void showDiagonalOfMatrix(int[][10], int, int, int, int);
+using namespace std;
+int main()
+{
+
+    int a[10][10];
+    int m, n;
+    cout << "Enter row  for Matrix A: " << endl;
+    cin >> m;
+    cout << "Enter columns  for Matrix A: " << endl;
+    cin >> n;
+    if (m == n)
+    {
+        cout << "Enter the elements of Matrix A: " << endl;
+        input(a, m, n);
+        cout << "Displaying the Matrix A: " << endl;
+        display(m, n, a);
+        cout << "Displaying the Diagonal Of A Matrix: " << endl;
+        showDiagonalOfMatrix(a, m, n, 0, 0);
+    }
+    else
+    {
+        cout << "Matrix is not square matrix" << endl;
+    }
+
+    return 0;
+}
+int input(int array[][10], int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cout << "arr[" << i << "][" << j << "] = ";
+            cin >> array[i][j];
+        }
+    }
+    return array[rows][10];
+}
+
+void display(int rows, int columns, int array[][10])
+{
+    for (int i = 0; i < rows; i++)
+    {
+        cout << "|";
+        for (int j = 0; j < columns; j++)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << "|" << endl;
+    }
+}
+void showDiagonalOfMatrix(int arr[][10], int rows, int columns, int i, int j)
+{
+
+    if (i == rows)
+    {
+        return;
+    }
+    if (j == columns)
+    {
+        cout << endl;
+         showDiagonalOfMatrix(arr, rows, columns, i + 1, 0);
+         return;
+    }
+    if (i == j)
+    {
+        cout << arr[i][j] << " ";
+    }
+    else
+    {
+        cout << " ";
+    }
+     showDiagonalOfMatrix(arr, rows, columns, i, j + 1);
+}
